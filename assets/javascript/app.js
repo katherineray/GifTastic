1b2eda1ed4f4701f81278e2e05acd936d4417153
Katherine Ray <katherine.ray@me.com>
2019-09-30 08:39:40 -0500

    updating file location html, creating button function app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function){
+
+    var array = ["giphyTBD"];
+
+    
+// Create function to make buttons to be added to page
+function createBottons(arrayToUse, classToAdd, areaToAddTo) {
+   $(areaToAddTo).empty();
+
+   
+}
+
+}
